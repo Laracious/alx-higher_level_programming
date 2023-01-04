8aaebbfcd9b64bfdbc329bc0be7ad15980696951
Laracious <omolaraamoke22@gmail.com>
2023-01-04 15:54:51 -0800

    Python - if/else, loops, function

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,4 @@
-#!/usr/bin/python3
-for i in range(97, 123):
-    if chr(i) != "e" and chr(i) != "q":
-        print(chr(i),end=" ")
-
+3-print_alphabt.py
+for letter in range(97, 123):
+    if chr(letter) is not 'q' and chr(letter) is not 'e':
+        print("{}".format(chr(letter)), end="")
