f1fa51c0dc20d6e6afcd45aa9b5c667c9a0b9e69
Laracious <omolaraamoke22@gmail.com>
2023-01-05 05:15:54 -0800

    Python - import & modules

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,7 +1,18 @@
-f __name__ == "__main__":
-        """Print the sum of 1 and 2."""
-            from add_0 import add
+#!/usr/bin/python3
+import sys
 
-                a = 1
-                    b = 2
-                        print("{} + {} = {}".format(a, b, add(a, b)))
+argv = sys.argv[1:]  # exclude the program name from the list of arguments
+num_args = len(argv)  # get the number of arguments
+
+# Print the number of arguments and the list of arguments
+if num_args == 0:
+    print("No argument.", end='\n\n')
+elif num_args == 1:
+    print("1 argument:", end='\n\n')
+else:
+    print(f"{num_args} arguments:", end='\n\n')
+
+# Print the position and value of each argument
+for i, arg in enumerate(argv, start=1):
+    print(f"{i}: {arg}", end='\n')
+
