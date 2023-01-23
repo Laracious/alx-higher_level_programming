afc685168cbc61a6f55c1cd582253422d9e2ffe8
Laracious <omolaraamoke22@gmail.com>
2023-01-23 08:03:50 -0800

    Task 8

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -15,4 +15,4 @@ def safe_function(fct, *args):
         return (result)
     except:
         print("Exception: {}".format(sys.exc_info()[1], file=sys.stderr)
-        return (none)
+        return (None)
---
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+def magic_calculation(a, b):
+    result = 0
+    for i in range(1, 3):
+        try:
+            if > a:
+                raise exception('Too far')
+            else:
+                redult += a ** b / i
+        except:
+                result = b + a
+                break
+        return (result)
+
