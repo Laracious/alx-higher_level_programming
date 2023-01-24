7d85333df708bead37d1a3da88c0736ec9e919c6
Laracious <omolaraamoke22@gmail.com>
2023-01-24 06:22:10 -0800

    3-square.py

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,40 +1,22 @@
 #!/usr/bin/python3
-"4-square.py define"
+"1-square.py define"
 
 
 class Square:
-    """class Square
+    """Class Square
     """
 
     def __init__(self, size=0):
-        """Inizialitation of variables
-        Arg self identificador
-        size tamañe of square
+        """Def
         """
-        self.size = size
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
 
     def area(self):
-        """Inizialitation of variables
-        Arg self identificador
+        """Def area
         """
         return self.__size ** 2
-
-    @property
-    def size(self):
-        """Inizialitation of variables
-        Arg self identificador
-        """
-        return self.__size
-
-    @size.setter
-    def size(self, value):
-        """Inizialitation of variables
-        Arg self identificador
-        value of square
-        """
-        if not isinstance(value, int):
-            raise TypeError("size must be an integer")
-        elif value < 0:
-            raise value("size must be >= 0")
-        else:
-            self.__size = value 
