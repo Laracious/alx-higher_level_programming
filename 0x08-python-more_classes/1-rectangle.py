3b1efb36848566e25eaea32cc306328199c70bef
Laracious <omolaraamoke22@gmail.com>
2023-01-30 05:56:23 -0800

    1-rectangle.py

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,29 +1,57 @@
 #!/usr/bin/python3
+"""class Rectangle that defines a rectangle
+    """
+
+
 class Rectangle:
+    """class rectangle
+    """
+
     def __init__(self, width=0, height=0):
-        self.width = width
+        """Initialize rectangle
+        """
+
         self.height = height
-
-    @property
-    def width(self):
-        return self._width
-
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self._width = value
+        self.width = width
 
     @property
     def height(self):
-        return self._height
+        """ Height Rectangle
+        """
+
+        return self.__height
 
     @height.setter
     def height(self, value):
+        """ Height Rectangle
+        """
+
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         if value < 0:
             raise ValueError("height must be >= 0")
-        self._height = value
+        self.__height = value
+
+    @property
+    def width(self):
+        """[summary]
+        Returns:
+            [type] -- [description]
+        """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """[summary]
+        Arguments:
+            value {[type]} -- [description]
+        Raises:
+            TypeError: [width must be an integer]
+            TypeError: [width must be >= 0]
+        """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
