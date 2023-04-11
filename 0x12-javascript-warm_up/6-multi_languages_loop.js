604104cadca2b4d9dbeee89d9e56c8f7c9ebd2bd
Laracious <omolaramoke22@gmail.com>
2023-04-11 14:20:58 -0700

    6-multi_languages_loop.js

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-
-// prints 3 lines
-
-const argss = ['C is fun', 'Python is cool', 'Javascript is amazing'];
+/*
+  print 3 lines with loop
+*/
+const args = ['C is fun', 'Python is cool', 'Javascript is amazing'];
 for (const line of args) {
   console.log(line);
 }
