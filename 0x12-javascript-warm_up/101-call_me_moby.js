09970c9b25ef259ee877968100b4faeae6d74511
Laracious <omolaramoke22@gmail.com>
2023-04-11 23:06:01 -0700

    101-call_me_moby.js

---
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+// executes x times a function
+
+const callMeMoby = function (x, theFunction) {
+	for (let times = x; times > 0; times--) {
+		theFunction();
+	}
+};
+
+export.callMeMoby = callMeMoby;
