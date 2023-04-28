970da2a994304a84bfc646662718bb16a119a1f4
Laracious <omolaramoke22@gmail.com>
2023-04-28 06:36:07 -0700

    0-hbtn_status.py

--- 0x11-python-network_1/4-hbtn_status.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" This module fetches a url and dispplays its response """
-if __name__ == "__main__":
-    import requests
-
-    res = requests.get("https://alx-intranet.hbtn.io/status")
-    print("Body response:")
-    print("\t- type: {}".format(type(res.text)))
-    print("\t- content: {}".format(res.text))
