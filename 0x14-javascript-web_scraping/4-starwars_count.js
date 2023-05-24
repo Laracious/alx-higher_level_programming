f30cf03fcca9eee52566c27657997969e7d6925d
Laracious <omolaramoke22@gmail.com>
2023-05-24 05:40:35 -0700

    4-starwars_count.js

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,8 +1,24 @@
 #!/usr/bin/node
-const fs = require('fs');
-const request = require('request');
-const args = process.argv.slice(2);
-const url = args[0];
-const fileName = args[1];
 
-request(url).pipe(fs.createWriteStream(fileName));
+const request = require('request');
+const starWarsUri = process.argv[2];
+let times = 0;
+
+request(starWarsUri, function (_err, _res, body) {
+  body = JSON.parse(body).results;
+
+  for (let i = 0; i < body.length; ++i) {
+    const characters = body[i].characters;
+
+    for (let j = 0; j < characters.length; ++j) {
+      const character = characters[j];
+      const characterId = character.split('/')[5];
+
+      if (characterId === '18') {
+        times += 1;
+      }
+    }
+  }
+
+  console.log(times);
+});
